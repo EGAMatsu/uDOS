b9c78d9e23b2976078b9d79e5e571dd4334aabba
superleaf1995 <jesusantonio30122016@gmail.com>
2022-01-15 12:41:28 -0600

    Redo the JCL build system to be more readable
    Signed-off-by: superleaf1995 <jesusantonio30122016@gmail.com>

--- build_mvs.bat
+++ build_mvs.bat
@@ -5,107 +5,41 @@ CLS
 
 CALL :IsInstalled 7z,"C:\Program Files\7-Zip"
 CALL :IsInstalled hercules,"C:\Program Files\Hercules\Hercules 3.07 (64 Bit)"
-CALL :IsInstalled gccmvs,%CD%\Toolchain
-CALL :IsInstalled runmvs,%CD%\Toolchain\mvs380
+CALL :IsInstalled runmvs,%CD%\toolchain\mvs380
 
-RMDIR /S /Q Distro
-MKDIR Distro Distro\kernel Distro\rtl Distro\Programs Distro\Tapes
-
-SET CFLAGS=-O2 -std=gnu99 -Itoolchain -DMACHINE=390 -ffreestanding
-
-ECHO ***************************************************************************
-ECHO uDOS Module and User Runtime Library
-ECHO ***************************************************************************
-(FOR %%I IN (rtl\*.c) DO (
-    ECHO %%~I
-
-    gccmvs %CFLAGS% -Irtl -S %%~I -o distro\rtl\%%~nI.asm
-    IF ERRORLEVEL 1 (
-        ECHO gccmvs returned %ERRORLEVEL%
-        EXIT /B 0
-    )
-))
-
-(FOR %%I IN (
-    rtl\*.asm
-) DO (
-    ECHO %%~I
-    COPY %%~I distro\rtl\%%~nI.asm
-))
-
-ECHO ***************************************************************************
-ECHO uDOS Programs and Utilities (for userland)
-ECHO ***************************************************************************
-(FOR %%I IN (programs\*.c) DO (
-    ECHO %%~I
-
-    gccmvs %CFLAGS% -Iprograms -Irtl -S %%~I -o distro\programs\%%~nI.asm
-    IF ERRORLEVEL 1 (
-        ECHO gccmvs returned %ERRORLEVEL%
-        EXIT /B 0
-    )
-))
-
-ECHO ***************************************************************************
-ECHO uDOS Platform Generic High-Level Kernel
-ECHO ***************************************************************************
-(FOR %%I IN (
-    kernel\*.c
-    kernel\comm\*.c
-    kernel\debug\*.c
-    kernel\fs\*.c
-    kernel\loader\*.c
-    kernel\mm\*.c
-) DO (
-    ECHO %%~I
-
-    gccmvs %CFLAGS% -Ikernel -S %%~I -o distro\kernel\%%~nI.asm
-    IF ERRORLEVEL 1 (
-        ECHO gccmvs returned %ERRORLEVEL%
-        EXIT /B 0
-    )
-))
-
-(FOR %%I IN (
-    kernel\*.asm
-) DO (
-    ECHO %%~I
-    COPY %%~I distro\kernel\%%~nI.asm
-))
+RMDIR /S /Q distro
+MKDIR distro distro\tapes
 
 ECHO Zipping all source files
-7z a -tzip Distro\DosKrnl.zip %CD%\Distro\kernel\*.asm
-7z a -tzip Distro\DosRtl.zip %CD%\Distro\rtl\*.asm
-7z a -tzip Distro\Programs.zip %CD%\Distro\Programs\*.asm
-7z a -tzip Distro\Jcl.zip %CD%\Jcl\*.jcl
-7z a -tzip Distro\All.zip %CD%\Distro\DosKrnl.zip %CD%\Distro\DosRtl.zip %CD%\Distro\Programs.zip %CD%\Distro\Jcl.zip
+7z a -tzip distro\source.zip %CD%\kernel\*.asm %CD%\kernel\*.c
+7z a -tzip distro\include.zip %CD%\kernel\*.h
+7z a -tzip distro\jcl.zip %CD%\jcl\*.jcl
+7z a -tzip distro\All.zip %CD%\distro\source.zip %CD%\distro\include.zip %CD%\distro\jcl.zip
 
-TYPE Jcl\TransferFiles.jcl >>Distro\TmpJcl.jcl
-TYPE Jcl\AsmRtl.jcl >>Distro\TmpJcl.jcl
-TYPE Jcl\AsmUtils.jcl >>Distro\TmpJcl.jcl
-TYPE Jcl\AsmKernel.jcl >>Distro\TmpJcl.jcl
-TYPE Jcl\LoadZero.jcl >>Distro\TmpJcl.jcl
-TYPE Jcl\Clean.jcl >>Distro\TmpJcl.jcl
+TYPE Jcl\transfer.jcl >>distro\alljcl.jcl
+TYPE Jcl\compile.jcl >>distro\alljcl.jcl
+TYPE Jcl\loadzero.jcl >>distro\alljcl.jcl
+TYPE Jcl\clean.jcl >>distro\alljcl.jcl
 ECHO Running MVS
-CALL runmvs Distro\TmpJcl.jcl Distro\Output.txt Distro\All.zip
+CALL runmvs distro\alljcl.jcl distro\output.txt distro\all.zip
 
 ECHO Extracting binary from tape
-hetget "Toolchain\mvs380\tapes\mftopc.het" Distro\kernel.bin 1
+hetget "Toolchain\mvs380\tapes\mftopc.het" distro\kernel.bin 1
 
 ECHO Creating final disk
-COPY Tools\stage1.txt Distro\stage1.txt
-COPY Tools\stage2.bin Distro\stage2.bin
-ECHO #File >Distro\Limine.cfg
-COPY DasdCtl.txt Distro\DasdCtl.txt
+COPY tools\stage1.txt distro\stage1.txt
+COPY tools\stage2.bin distro\stage2.bin
+ECHO #File >distro\Limine.cfg
+COPY dasdctl.txt distro\dasdctl.txt
 
-CD Distro
-DEL SYSDSK00.cckd
-dasdload -bz2 ..\DasdCtl.txt SYSDSK00.cckd
+CD distro
+DEL sysdsk00.cckd
+dasdload -bz2 ..\dasdctl.txt sysdsk00.cckd
 CD ..
 
-COPY Toolchain\mvs380\tapes\*.het Distro\Tapes\
+COPY toolchain\mvs380\tapes\*.het distro\tapes\
 
-hercules -f hercules.cnf >Distro\Log.txt
+hercules -f hercules.cnf >distro\log.txt
 EXIT /B 0
 
 REM Function to check for installed program
